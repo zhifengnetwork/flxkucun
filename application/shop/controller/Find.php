b6701befa76a216812db860ec2dd50861ef93887
yezongxiu <yzx>
2019-06-14 16:55:55 +0800

    视频点赞功能开发

--- application/shop/controller/Find.php
+++ application/shop/controller/Find.php
@@ -5,12 +5,19 @@ use think\Db;
 class Find extends MobileBase{
     // 跳转到发现页面
     public function find(){
-
+        $user_id = session('user.user_id');
     	//平台商品视频
         $goodsVideoList = Db::table('tp_goods')->where(['is_on_sale'=>1,'is_video'=>1,'is_recommend'=>1])->field('goods_name,goods_id,original_img,video')->order("sort DESC")->limit(4)->select();
+        $addtime=strtotime(date("Y-m-d"));
         foreach ($goodsVideoList as $key => $value){
-                 $videoImg=explode('.',$value['video']);
-                 $goodsVideoList[$key]["original_img"]=$videoImg[0].".jpg";
+            $res = Db::name('video_favor')->where(['addtime'=>$addtime,'user_id'=>$user_id,'type'=>1,'video_id'=>$value['goods_id']])->find();
+              if($res){
+                  $goodsVideoList[$key]["favor"]="red";
+              }else{
+                  $goodsVideoList[$key]["favor"]="";
+              }
+              $videoImg=explode('.',$value['video']);
+             $goodsVideoList[$key]["original_img"]=$videoImg[0].".jpg";
         }
         $this->assign('goodsVideo', $goodsVideoList);
 
@@ -18,6 +25,12 @@ class Find extends MobileBase{
     	$userVideoList = Db::table('tp_video')->where(['status'=>1,'is_on_sale'=>1,'is_recommend'=>1])->field('id,title,nickname,video_img,user_id')->order("sort DESC")->limit(8)->select();
     	foreach ($userVideoList as $key => $value){
     	    $userImg=Db::table('tp_users')->where(['user_id'=>$value['user_id']])->field('head_pic')->find();
+            $res = Db::name('video_favor')->where(['addtime'=>$addtime,'user_id'=>$user_id,'type'=>2,'video_id'=>$value['id']])->find();
+            if($res){
+                $userVideoList[$key]["favor"]="red";
+            }else{
+                $userVideoList[$key]["favor"]="";
+            }
             $userVideoList[$key]['head_pic']=$userImg['head_pic'];
         }
     	$this->assign('userVideo',$userVideoList);
@@ -27,27 +40,41 @@ class Find extends MobileBase{
     //商品视频查找更多
 
     public function find_more(){
-
+        $user_id = session('user.user_id');
         //平台商品视频
         $goodsVideoList = Db::table('tp_goods')->where(['is_on_sale'=>1,'is_video'=>1,'is_recommend'=>1])->field('goods_name,goods_id,original_img,video')->order("sort DESC")->limit(10)->select();
+        $addtime=strtotime(date("Y-m-d"));
         foreach ($goodsVideoList as $key => $value){
             $videoImg=explode('.',$value['video']);
             $goodsVideoList[$key]["original_img"]=$videoImg[0].".jpg";
+            $res = Db::name('video_favor')->where(['addtime'=>$addtime,'user_id'=>$user_id,'type'=>1,'video_id'=>$value['goods_id']])->find();
+            if($res){
+                $goodsVideoList[$key]["favor"]="red";
+            }else{
+                $goodsVideoList[$key]["favor"]="";
+            }
         }
         $this->assign('goodsVideo', $goodsVideoList);
         return $this -> fetch();
     }
    //ajax加载商品视频
     public function ajaxGoodsList(){
-
+        $user_id = session('user.user_id');
         $page =  I('get.page');
         $pageSize=10;
         $pageOffset=($page-1)*$pageSize;
         //平台商品视频
         $goodsVideoList = Db::table('tp_goods')->where(['is_on_sale'=>1,'is_video'=>1,'is_recommend'=>1])->field('goods_name,goods_id,original_img,video')->order("sort DESC")->limit($pageOffset,$pageSize)->select();
+        $addtime=strtotime(date("Y-m-d"));
         foreach ($goodsVideoList as $key => $value){
             $videoImg=explode('.',$value['video']);
             $goodsVideoList[$key]["original_img"]=$videoImg[0].".jpg";
+            $res = Db::name('video_favor')->where(['addtime'=>$addtime,'user_id'=>$user_id,'type'=>1,'video_id'=>$value['goods_id']])->find();
+            if($res){
+                $goodsVideoList[$key]["favor"]="red";
+            }else{
+                $goodsVideoList[$key]["favor"]="";
+            }
         }
         $data='';
         foreach ($goodsVideoList as $value){
@@ -59,7 +86,7 @@ class Find extends MobileBase{
 									<img src=\"__STATIC__/images/home_zp/playBtn-sm.png\" />
 								</span>
 							</div>
-							<p>".$value["goods_name"]."</p>
+							<p>".$value["goods_name"]." <i class=\"video_favor ".$value["favor"]."\"></i></p>
 						</a>
 			</li>";
         }
@@ -67,17 +94,24 @@ class Find extends MobileBase{
     }
   //ajax加载用户视频
     public function ajaxUserVideo(){
-
+        $user_id = session('user.user_id');
         $page =  I('get.page');
         $pageSize=8;
         $pageOffset=($page-1)*$pageSize;
         //平台推荐用户视频
         $userVideoList = Db::table('tp_video')->where(['status'=>1,'is_on_sale'=>1,'is_recommend'=>1])->field('id,title,nickname,video_img,user_id')->order("sort DESC")->limit($pageOffset,$pageSize)->select();
+        $addtime=strtotime(date("Y-m-d"));
         foreach ($userVideoList as $key => $value){
             $userImg=Db::table('tp_users')->where(['user_id'=>$value['user_id']])->field('head_pic')->find();
             $userVideoList[$key]['head_pic']=$userImg['head_pic'];
             if(empty($value['video_img'])){
                 $userVideoList[$key]['video_img']="/template/shop/rainbow/static/images/home_zp/00product-img01.png";
+            }
+            $res = Db::name('video_favor')->where(['addtime'=>$addtime,'user_id'=>$user_id,'type'=>2,'video_id'=>$value['id']])->find();
+            if($res){
+                $userVideoList[$key]["favor"]="red";
+            }else{
+                $userVideoList[$key]["favor"]="";
             }
         }
         $data='';
@@ -91,7 +125,7 @@ class Find extends MobileBase{
 								<img src=\"__STATIC__/images/home_zp/playBtn-sm.png\" />
 							</span>
 						</div>
-						<p>".$value["title"]."</p>
+						<p>".$value["title"]."<i class=\"video_favor ".$value["favor"]."\"></i></p>
 						<div class=\"userInfo\">
 						<span class=\"acatav\"><img src=".$value["head_pic"]."></span>
 							<span class=\"name\">".$value["nickname"]."</span>
