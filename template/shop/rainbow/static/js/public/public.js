9351d60d09f49dfcddbd3b496f25477691ac4e54
webhzp <1225613448@qq.com>
2019-06-24 18:48:57 +0800

    个人中心页&首页样式调整、免单团倒计时完善、详情页底部操作栏布局&收藏效果的实现、首页&发现页轮播视频改为单个视视频

--- template/shop/rainbow/static/js/public/public.js
+++ template/shop/rainbow/static/js/public/public.js
@@ -19,23 +19,69 @@ function pageJump(_url){
 }
 
 
+/**
+ * 详情页收藏
+ */
+$(".action-bar .fav").click(function(){
+	var that = $(this);
+	if(that.hasClass("faved")){
+			// 已收藏 -> 取消收藏
+		$(this).removeClass("faved");  
+		$(".fav-tips").show().html("<em class='fav-animation'>取消收藏</em>");        
+	}else{
+		// 未收藏 -> 收藏
+		$(this).addClass("faved");
+		$(".fav-tips").show().html("<em class='fav-animation'>收藏成功</em>");    
+	}
+});
 
-$(function(){
-	/**
-	 * 详情页收藏
-	 */
-	$(".action-bar .fav").click(function(){
-		var that = $(this);
-		if(that.hasClass("faved")){
-			 // 已收藏 -> 取消收藏
-			$(this).removeClass("faved");  
-			$(".fav-tips").show().html("<em class='fav-animation'>取消收藏</em>");        
-		}else{
-			// 未收藏 -> 收藏
-			$(this).addClass("faved");
-			$(".fav-tips").show().html("<em class='fav-animation'>收藏成功</em>");    
-		}
-	});
+/**
+ * 详情-规格选择
+ */
+function skuShow(){
+	$(".mask").show();
+	$(".sku-content").animate({'bottom':'0'});
+	stopSliding(".wrapper");//禁止body滑动
+}
 
-})
+function skuHide(){
+	$(".mask").hide();
+	$(".sku-content").animate({'bottom':'-100%'});
+	resumingSlip(".wrapper");//恢复body滑动
+}
 
+/**
+ * 取消冒泡事件
+ */
+function cancelTap(event){
+	var e = window.event || event;
+	if(e.stopPropagation){
+		e.stopPropagation();
+	}else{
+		e.cancelBubble = true;
+	}
+}
+
+/**
+ * 
+ * 禁止body滑动
+ */
+function stopSliding(name){
+	var $name = $(name);
+	var scrollTop = document.body.scrollTop;//设置背景元素的位置
+	$name.attr('data-top',scrollTop);
+	$name.css("position","fixed");//设置背景元素的position属性为‘fixed'
+	$name.css("top","-" + scrollTop + "px");
+}
+
+/**
+ * 
+ * 恢复body滑动
+ */
+function resumingSlip(name){
+	var $name = $(name);
+	var scrollTop = $name.attr('data-top');//设置背景元素的位置
+	$name.css("top","0px");//恢复背景元素的初始位置
+	$name.css("position","static");//恢复背景元素的position属性
+	$(document).scrollTop(scrollTop);//scrollTop,设置滚动条的位置
+}
