5554aa54fc1a4290df3055c5ef3594466138e25e
xiaozhi9752 <975247129@qq.com>
2019-06-26 14:41:09 +0800

    ts

--- application/common/logic/Pay.php
+++ application/common/logic/Pay.php
@@ -314,7 +314,7 @@ class Pay
      * @return $this
      */
     public function delivery($district_id){
-        pred($this->payList[0]);
+        pred($this->payList[0]->goods_name);
         if (isset($this->payList[0]['goods']['is_virtual']) && $this->payList[0]['goods']['is_virtual'] == 0) {
             if (empty($this->shop) && empty($district_id)) {
                 throw new TpshopException("计算订单价格", 0, ['status' => -1, 'msg' => '请填写收货信息', 'result' => ['']]);
