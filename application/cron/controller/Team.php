87b26a3362fe25ba3fe4b412fd9d420044ccfcb1
luyu627 <152939118@qq.com>
2019-07-04 17:31:47 +0800

    Update Team.php

--- application/cron/controller/Team.php
+++ application/cron/controller/Team.php
@@ -39,7 +39,7 @@ class Team extends Controller{
         //取结束时间大于等于当前时间且未结束的团购
         $GroupBuy = M('group_buy');
         $list = $GroupBuy->field('id,buy_num,min_mduser_num')->where(['end_time'=>['elt',time()],'is_end'=>0])->select();
-
+        
         $list1 = $GroupBuy->field('goods_id')->where(['end_time'=>['elt',time()],'is_end'=>0])->select();
         $GroupBuy->where(['end_time'=>['elt',time()],'is_end'=>0])->update(['is_end'=>1]);
 
@@ -55,10 +55,10 @@ class Team extends Controller{
         $AccountLog = M('account_log');
         foreach($list as $v){ //订单中source_uid最多的用户免单
             if($v['buy_num'] < $v['min_mduser_num'])continue;
-            $info = $Order->field('source_uid,count(source_uid) as num')->where(['prom_id'=>$v['id'],'prom_type'=>2,'source_uid'=>['neq',0]])->group('source_uid')->order('num desc,pay_time desc')->limit(1)->select();
+            $info = $Order->field('source_uid,count(source_uid) as num')->where(['prom_id'=>$v['id'],'prom_type'=>2,'source_uid'=>['neq',0]])->group('source_uid')->order('num desc,pay_time desc')->limit(1)->find();
             if($info){
                 $orderinfo = $Order->field('order_id,order_sn,shipping_price,total_amount')->where(['user_id'=>$info['source_uid'],'pay_status'=>1,'prom_id'=>$v['id'],'prom_type'=>2])->find();
-                $Users->where(['user_id'=>$info['source_uid']])->setInc('user_money',$orderinfo['total_amount']);    
+                $Users->where(['user_id'=>$info['source_uid']])->setInc('user_money',$orderinfo['total_amount']);
                 $AccountLog->add(['user_id'=>$info['source_uid'],'user_money'=>$orderinfo['total_amount'],'change_time'=>time(),'desc'=>'团购分享最多获得免单','order_sn'=>$orderinfo['order_sn'],'order_id'=>$orderinfo['order_id'],'log_type'=>80]);
             }
         }
